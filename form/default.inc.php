dfa26bfcb7408536e3743a83e9d4636601a3f208
Nehemiah Torres <nehemiah.torres@ufl.edu>
2018-07-29 22:43:05 -0400

    added form processor line to index added default form added error form added thank you form

---
+++ form/default.inc.php
@@ -0,0 +1,13 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: bahnschrift;
+}
+</style>
+
+<div class="container">
+  <h2>Sorry but something went wrong with your submission.</h2>
+  <p>Please try again.</p>
+</div>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,6 @@
+<div class="container">
+  <img src="../images/euskal-herria.png" alt="Euskal Herria" title="Euskal Herria" width="500px" />
+  <h1>Thank you for your enquiry</h1>
+  <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+
+</div>
